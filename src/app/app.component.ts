d4c790dbf0253bdfb07a2744a0d65043adb402f9
fr0ziii <fr0ziisc2@gmail.com>
2022-04-08 21:38:41 +0100

    formularioAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formAngular';
+  
+  employeeArray: Employee[] = [
+    {id: 1, name: "test", country: "test"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if(this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+
+  openEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  delete() {
+    if(confirm('¿Estás seguro de eliminar el empleado?'))
+    this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
 }
